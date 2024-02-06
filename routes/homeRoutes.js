f524799965899c6c60fe4ff59340fa78ce61d2c4
Monis <monisdl.2005@gmail.com>
2024-02-06 09:48:37 +0530

    message

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,13 +2,8 @@ const express  = require("express");
 
 const router = express.Router();
 
-router.get("/",function(req,res){
-    if(req.session.isAuth){
-        return res.render("home");
-    }
-    else{
-        return res.redirect("/login");
-    }
+router.get("/",function(req,res){ 
+    return res.render("home");
 });
 
 router.get("/send", function(req,res){
